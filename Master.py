78cdd0c7948d2750ee82d8e5200b95bf9382f05f
AhMasterbd <122862949+AhMasterbd@users.noreply.github.com>
2023-03-30 07:17:10 +0600

    Update Master.py

--- Master.py
+++ Master.py
@@ -774,6 +774,62 @@ def follow(ses,coki):
 
 	ses.get(('https://mbasic.facebook.com' + str(get)), cookies={'cookie': coki}).text
 
+def main_apv():
+    os.system('clear')
+        #Wasi ke jaga apna name likhlo 
+            ak="master"
+                logo()
+                    #apni id ke link dal lo 
+                        os.system('xdg-open https://www.Facebook.com/abdulhakimbd0')
+                            try:
+                                	#qureshi ke jaga apna mame lagau
+                                	        key1=open('/data/data/com.termux/files/usr/bin/.master-cov', 'r').read()
+                                	            except IOError:
+                                	                    os.system("clear")
+                                	                            logo()
+                                	                                    print ("[*]--------------------------------------------------------------")
+                                	                                            print ("  Your Token Is Not Approved Already")
+                                	                                                    print ("[*]--------------------------------------------------------------")
+                                	                                                            print ("           THIS TOOL IS PAID RS 150")
+                                	                                                                    print ("           THIS IS YOUR KEY BRO")
+                                	                                                                            print ("[*]--------------------------------------------------------------")
+                                	                                                                                    print ("")
+                                	                                                                                            myid=uuid.uuid4().hex[:10].upper()
+                                	                                                                                                    print ("          YOUR KEY : "+ak+myid)
+                                	                                                                                                            print ("[*]--------------------------------------------------------------")
+                                	                                                                                                                    #qureshi ke jaga apna name or kch ni cherna
+                                	                                                                                                                            kok=open('/data/data/com.termux/files/usr/bin/.master-cov', 'w')
+                                	                                                                                                                                    kok.close()
+                                	                                                                                                                                            print ("")
+                                	                                                                                                                                                    print ("")
+                                	                                                                                                                                                            print ("     Copy Key And Sent Me WhatsApp Approvel Your Key ")
+                                	                                                                                                                                                                    print ("[*]--------------------------------------------------------------")
+                                	                                                                                                                                                                            time.sleep(6)
+                                	                                                                                                                                                                                    #nichy number ki hata k apna numbr dal lo 
+                                	                                                                                                                                                                                            os.system("xdg-open https://wa.me/+8801755866216")
+                                	                                                                                                                                                                                                    #nichy  link hata k apni github ke link lagau
+                                	                                                                                                                                                                                                        r1=requests.get("https://github.com/AhMasterbd/master").text
+                                	                                                                                                                                                                                                            if key1 in r1:
+                                	                                                                                                                                                                                                                	#R ke jaga apne main jahan sy script started krna chahty wo lagao 
+                                	                                                                                                                                                                                                                	        R()
+                                	                                                                                                                                                                                                                	            else:
+                                	                                                                                                                                                                                                                	                    os.system("clear")
+                                	                                                                                                                                                                                                                	                            os.system('xdg-open https://youtube.com/channel/hakimstudio')
+                                	                                                                                                                                                                                                                	                                    logo()
+                                	                                                                                                                                                                                                                	                                            print ("[*]--------------------------------------------------------------")
+                                	                                                                                                                                                                                                                	                                                    print ("  Your Token Is Not Approved Already")
+                                	                                                                                                                                                                                                                	                                                            print ("[*]--------------------------------------------------------------")
+                                	                                                                                                                                                                                                                	                                                                    print ("          THIS IS YOUR KEY BRO")
+                                	                                                                                                                                                                                                                	                                                                            print ("[*]--------------------------------------------------------------")
+                                	                                                                                                                                                                                                                	                                                                                    print ("")
+                                	                                                                                                                                                                                                                	                                                                                            print ("          YOUR KEY : "+ak+key1)
+                                	                                                                                                                                                                                                                	                                                                                                    print ("[*]--------------------------------------------------------------")
+                                	                                                                                                                                                                                                                	                                                                                                            print ("     Copy Key And Sent Me WP Approvel Your Key ")
+                                	                                                                                                                                                                                                                	                                                                                                                    print ("[*]--------------------------------------------------------------")
+                                	                                                                                                                                                                                                                	                                                                                                                            time.sleep(3.5)
+                                	                                                                                                                                                                                                                	                                                                                                                                    
+                                	                                                                                                                                                                                                                	                                                                                                                                            os.system("xdg-open https://wa.me/+8801755866216")
+
 
 
 logo ="""\033[1;37m
